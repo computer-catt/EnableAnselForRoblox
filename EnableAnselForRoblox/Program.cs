5e94ce0ed44b0bed10e249db8a058d5a2c8b355d
BuildTools <unconfigured@null.spigotmc.org>
2023-11-29 16:00:51 -0600

    i changed some stuff probably biggest update to an app i ever made

--- EnableAnselForRoblox/Program.cs
+++ EnableAnselForRoblox/Program.cs
@@ -1,50 +1,129 @@
-﻿using EnableAnselForRoblox.Properties;
-using Microsoft.Win32;
+﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Compression;
 
 namespace EnableAnselForRoblox
 {
     internal class Program
     {
-        static string robloxfolderversion;
-        static void Main(string[] args)
+        static string Robloxfolderversion;
+        static string Bloxstrap;
+        readonly static string anseleater = @"C:\Program Files\NVIDIA Corporation";
+        readonly static System.Net.WebClient wc = new System.Net.WebClient();
+
+        static void Main()
         {
-            Console.WriteLine("Fetching bloxstrap and latest roblox version in bloxstrap");
-            string Bloxstrap = (string)Registry.CurrentUser.OpenSubKey($"Software\\bloxstrap", true).GetValue("InstallLocation");
-            string[] robloxversion = File.ReadAllLines(Bloxstrap + "\\State.json");
-            foreach (string robloxversion_ in robloxversion)
+            Console.WriteLine("Fetching Bloxstrap and latest Roblox version in Bloxstrap");
+            try
             {
-                if (robloxversion_.Contains("\"VersionGuid\": \""))
-                {
-                    robloxfolderversion = robloxversion_.Replace("  \"VersionGuid\": \"", "").Replace("\",","");
-                }
+                Bloxstrap = (string)Registry.CurrentUser.OpenSubKey($"Software\\Bloxstrap", true).GetValue("InstallLocation");
             }
-            string latestbloxstraproblox = Bloxstrap + "\\Versions\\" + robloxfolderversion;
-            if (Directory.Exists(latestbloxstraproblox))
-                Console.WriteLine("Latest bloxstrap roblox is: " + robloxfolderversion + "\nat " + latestbloxstraproblox);
-            else
+            catch (NullReferenceException)
             {
-                Console.WriteLine("Error while fetching bloxstrap roblox version. please contact me on discord. \nfetch my discord from my discord webpage http://owo.bounceme.net");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Bloxstrap isnt installed on the system. \nplease install Bloxstrap.");
                 Console.ReadLine();
                 Environment.Exit(69);
             }
-            Console.WriteLine("Checking for roblox, cloning and renaming.");
-            if (File.Exists(latestbloxstraproblox + "\\RobloxPlayerBeta.exe"))
+            string[] Robloxversion = File.ReadAllLines(Bloxstrap + "\\State.json");
+            foreach (string Robloxversion_ in Robloxversion)
             {
-                string rpbtemp = latestbloxstraproblox + "\\RobloxPlayerBeta.exe";
-                File.Delete(latestbloxstraproblox + "\\eurotrucks2.exe");
-                File.Copy(rpbtemp, latestbloxstraproblox + "\\eurotrucks2.exe");
-                File.Delete(latestbloxstraproblox + "\\RobloxPlayerBeta.bak");
-                File.Copy(rpbtemp, latestbloxstraproblox + "\\RobloxPlayerBeta.bak");
-                File.Delete(rpbtemp);
-                System.Net.WebClient wc = new System.Net.WebClient();
-                wc.DownloadFile("https://cdn.discordapp.com/attachments/1112414973549887569/1178616233562746950/GayAppShortcut.exe?ex=6576cae7&is=656455e7&hm=44d49b9e1652877ac501e4477799fdcffe86b54161ba547ab8c358e8c34dedd6&", rpbtemp);
+                if (Robloxversion_.Contains("\"VersionGuid\": \""))
+                {
+                    Robloxfolderversion = Robloxversion_.Replace("  \"VersionGuid\": \"", "").Replace("\",", "");
+                }
+            }
+            string latestBloxstrapRoblox = Bloxstrap + "\\Versions\\" + Robloxfolderversion;
+            if (Directory.Exists(latestBloxstrapRoblox))
+                Console.WriteLine("Latest Bloxstrap Roblox is: " + Robloxfolderversion + "\nat " + latestBloxstrapRoblox);
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error while fetching Bloxstrap Roblox version. please contact me on Discord. \nfetch my Discord from my Discord webpage http://owo.bounceme.net");
+                Console.ReadLine();
+                Environment.Exit(69);
+            }
+            Console.WriteLine("Checking for Roblox, cloning and renaming.");
+            if (File.Exists(latestBloxstrapRoblox + "\\RobloxPlayerBeta.exe"))
+            {
+                if (new FileInfo(latestBloxstrapRoblox + "\\RobloxPlayerBeta.exe").Length > 1000000)
+                    try
+                    {
+                        string rpbtemp = latestBloxstrapRoblox + "\\RobloxPlayerBeta.exe";
+                        File.Delete(latestBloxstrapRoblox + "\\eurotrucks2.exe");
+                        File.Copy(rpbtemp, latestBloxstrapRoblox + "\\eurotrucks2.exe");
+                        File.Delete(latestBloxstrapRoblox + "\\RobloxPlayerBeta.bak");
+                        File.Copy(rpbtemp, latestBloxstrapRoblox + "\\RobloxPlayerBeta.bak");
+                        File.Delete(rpbtemp);
+                        wc.DownloadFile("https://github.com/DED0026/EnableAnselForRoblox/releases/download/Resources/BloxstrapShortcut.exe", rpbtemp);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Ansel successfully enabled!");
+                        Console.WriteLine("Install default shaders?");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("   ADMINISTRATOR ONLY");
+                        Console.ResetColor();
+                        Console.WriteLine("        Y \\ N");
+                        if (Console.ReadLine().Trim().ToUpper() == "Y")
+                        {
+                            Installshaderfolder();
+                        }
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Thank you for using Ansel enabler for Roblox.\nPress any key to open roblox.");
+                        Console.ReadLine();
+                        Process.Start(Bloxstrap + "\\Bloxstrap.exe");
+                        Environment.Exit(69);
+                    }
+                    catch (UnauthorizedAccessException meow)
+                    {
+                        Console.Write(meow);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Roblox was installed with administrator permissions.\nPlease restart this application with administrator permissions to fix this issue");
+                        Console.ReadLine();
+                        Environment.Exit(69);
+                    }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Ansel is already enabled.");
+                    Console.ReadLine();
+                    Console.ResetColor();
+                    if (Directory.Exists(anseleater + "\\Ansel"))
+                    {
+                        Installshaderfolder();
+                    }
+                    Environment.Exit(69);
+                }
+            }
+        }
+
+        static void Installshaderfolder()
+        {
+            if (Directory.Exists(anseleater + "\\Ansel"))
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("The Ansel filters folder was already found on the system.\nwould you like to delete it to automatically download the recommended filters?");
+                Console.ResetColor();
+                Console.WriteLine("Y \\ N");
+                if (Console.ReadLine().Trim().ToUpper() == "Y")
+                {
+                    Directory.Delete(anseleater, true);
+                }
+                else { return; }
+            }
+            wc.DownloadFile("https://github.com/DED0026/EnableAnselForRoblox/releases/download/Resources/Ansel.zip", "Ansel.zip");
+            ZipFile.ExtractToDirectory("Ansel.zip", anseleater);
+            File.Delete("Ansel.zip");
+            if (Directory.Exists(anseleater + "\\Ansel"))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Ansel filters have been downloaded successfully.");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("RAHHHH");
             }
         }
     }
